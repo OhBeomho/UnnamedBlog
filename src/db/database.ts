322207b6da423be12497020223c335702be57497
OhBeomho <tyler0322@naver.com>
2023-01-23 19:28:51 +0900

    non production debug mode

--- src/db/database.ts
+++ src/db/database.ts
@@ -2,9 +2,15 @@ import "dotenv/config";
 import mongoose from "mongoose";
 
 export function connect(callback: () => void) {
+	const isProduction = process.env.NODE_ENV === "production";
+
+	if (!isProduction) {
+		mongoose.set("debug", true);
+	}
+
 	mongoose.set('strictQuery', true);
 	mongoose.connect(String(process.env.DB_CONN_STRING), {
-		dbName: "main"
+		dbName: isProduction ? "main" : "test"
 	}).then(() => {
 		console.log("Connected to database.");
 		callback();
